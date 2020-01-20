05baa1bc4c40be5d229a23e1ad0c534e617f5e69
Vladimir Doktorov <vladimir.doktorov@felinesoft.com>
2020-01-20 12:34:02 +0000

    initial commit

---
+++ EDDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,18 @@
+﻿using EDDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options) { }
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
